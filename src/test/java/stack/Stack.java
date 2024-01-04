7c462b17defff262cd5715cc6645ddcbcf467750
sendkite <sendkite1@gmail.com>
2024-01-04 21:32:07 +0900

    feat: create empty stack

---
+++ src/test/java/stack/Stack.java
@@ -0,0 +1,14 @@
+package stack;
+
+public class Stack {
+    private boolean empty = true;
+    private int size = 0;
+
+    public boolean isEmpty() {
+        return empty;
+    }
+
+    public int getSize() {
+        return size;
+    }
+}
--- src/test/java/stack/StackTest.java
+++ src/test/java/stack/StackTest.java
@@ -2,10 +2,16 @@ package stack;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class StackTest {
 
     @Test
-    void nothing() throws Exception {
+    void newlyCreateStack_shouldBeEmpty() throws Exception {
 
+        Stack stack = new Stack();
+        assertTrue(stack.isEmpty());
+        assertEquals(0, stack.getSize());
     }
 }
